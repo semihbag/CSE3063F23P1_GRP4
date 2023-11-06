a4123f3dccd413e1f2f25a55bb9b8cf67ca25c25
elifekcby <149164383+elifekocabey@users.noreply.github.com>
2023-11-06 13:46:20 +0300

    elife branch

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(){
-        System.out.println("hello");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!2");
     }
 }
