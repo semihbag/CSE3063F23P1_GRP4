fd218f30ad0927cba60a90478cfd6dd911152ca0
semihbag <semih702bag@gmail.com>
2023-11-13 21:26:34 +0300

    hello world

---
+++ src/pagePackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package pagePackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi barbie hi ken");
+	}
+
+}
