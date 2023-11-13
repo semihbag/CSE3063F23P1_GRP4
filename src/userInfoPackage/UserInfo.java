0707c62f390c99a64c81fe264ad7c35c80ee983b
semihbag <semih702bag@gmail.com>
2023-11-13 22:17:09 +0300

    userinfo added

--- src/userInfoPackage/UserInfo.java
+++ src/userInfoPackage/UserInfo.java
@@ -1,5 +1,30 @@
 package userInfoPackage;
 
 public class UserInfo {
+	private String username;
+	private String password;
+	
+	public UserInfo(String username, String password) {
+		setUsername(username);
+		setPassword(password);
+	}
+	
+	/// GETTER - SETTER METHODS
+	public String getUsername() {
+		return username;
+	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
