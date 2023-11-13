4597f9be8c5fb6e241a18a3827a6fb1c6f9ac2ac
semihbag <semih702bag@gmail.com>
2023-11-13 21:24:23 +0300

    interface building

--- src/pagePackage/UserInterface.java
+++ src/pagePackage/UserInterface.java
@@ -1,5 +1,33 @@
 package pagePackage;
 
-public class UserInterface {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class UserInterface {
+	
+	private Scanner consoleScanner;
+	private ArrayList<Page> pages;
+	private Page currentPage;
+
+	
+	// constructor
+	public UserInterface() {
+		this.consoleScanner = new Scanner(System.in);
+		this.pages = new ArrayList<>();
+	}
+	
+	// this function calls runPage func of the current page
+	public void display() {
+		currentPage.runPage();
+	}
+
+	// to change displayed page
+	// if the pages list contains the selected page, replace the page and return true
+	// if not return false
+	public boolean setCurrentPage(PageType pageType) {
+		
+		
+		
+		return true;
+	}
 }
