0642fbffe32038b5306b5b4fc5d55f1626f0e8bb
semihbag <semih702bag@gmail.com>
2023-11-14 00:37:36 +0300

    test class added

--- src/pagePackage/Test.java
+++ src/pagePackage/Test.java
@@ -1,14 +1,29 @@
 package pagePackage;
 
+import java.util.Scanner;
+
+import userInterfacePackage.*;
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		UserInterface ui = new UserInterface();
+		Scanner scanner = new Scanner(System.in);
+		String content = "kurs kayıt sistemine hoşgeldiniz lütfen username ve password giriniz";
+		LoginPage login = new LoginPage(scanner, PageType.LOGIN_PAGE, "Login Page", content);
+		
+		boolean a = ui.addPage(login);
+		System.out.println(a);
+		
+		boolean b = ui.setCurrentPage(PageType.LOGIN_PAGE);
+		System.out.println(b);
 		
 		ui.display();
 		
+	
+		login.deneme();
+		System.out.println("test basarili");
 	}
 
 }
