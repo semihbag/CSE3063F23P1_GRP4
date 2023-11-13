104a33e2cd0cc305e4eb6a95accc056919a82ce0
semihbag <semih702bag@gmail.com>
2023-11-13 21:24:11 +0300

    page building

--- src/pagePackage/Page.java
+++ src/pagePackage/Page.java
@@ -1,5 +1,76 @@
 package pagePackage;
 
-public abstract class Page {
+import java.util.Scanner;
 
+public abstract class Page {
+ 
+	private Scanner consoleScanner;
+	private PageType type;
+	private String name;
+	private String content; 
+	
+	
+	// methods
+	// constructor
+	public Page(Scanner consoleScanner, PageType type, String name, String content) {
+		setConsoleScanner(consoleScanner);
+		setType(type);
+		setName(name);
+		setContent(content);
+	}
+	
+	// the abstract method, this will be build later 
+	public abstract void runPage();
+	
+	// this function print the content
+	private void showContent() {
+		System.out.println(getContent());
+	}
+	
+	// 
+	private String takeInput() {
+		String inp = consoleScanner.nextLine();
+		return inp;
+	}
+
+	public Scanner getConsoleScanner() {
+		return consoleScanner;
+	}
+
+	public void setConsoleScanner(Scanner consoleScanner) {
+		this.consoleScanner = consoleScanner;
+	}
+
+	public PageType getType() {
+		return type;
+	}
+
+	public void setType(PageType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+	
+	// GETTER - SETTER METHODS
+	
+	
+	
 }
