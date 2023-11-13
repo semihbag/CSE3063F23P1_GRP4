bf6eafbcfe08f0bc47cc1b8b6e62c038b20a5468
semihbag <semih702bag@gmail.com>
2023-11-14 00:36:40 +0300

    system added into pages

--- src/pagePackage/Page.java
+++ src/pagePackage/Page.java
@@ -3,7 +3,7 @@ package pagePackage;
 import java.util.Scanner;
 
 public abstract class Page {
- 
+//	private System system;
 	private Scanner consoleScanner;
 	private PageType type;
 	private String name;
@@ -21,19 +21,19 @@ public abstract class Page {
 	public abstract void runPage();
 	
 	// this function print the content
-	private void showContent() {
+	public void showContent() {
 		System.out.println(getContent());
 	}
 	
 	// 
-	private String takeInput() {
+	public String takeInput() {
 		String inp = consoleScanner.nextLine();
 		return inp;
 	}
 
-	public Scanner getConsoleScanner() {
-		return consoleScanner;
-	}
+	
+	
+	
 
 	public void setConsoleScanner(Scanner consoleScanner) {
 		this.consoleScanner = consoleScanner;
