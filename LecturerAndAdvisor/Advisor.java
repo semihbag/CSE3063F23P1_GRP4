5c632a220c98809a1708aa9b5cb40f2dfcbf1891
erenncetin <147340241+erenncetin@users.noreply.github.com>
2023-11-14 16:38:55 +0000

    findAwaitingStudents method is created

---
+++ LecturerAndAdvisor/Advisor.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Advisor extends Lecturer {
+    
+    private ArrayList<Student> students;
+    private ArrayList<Student> awaitingStudents;
+    private Password password;
+
+    // Constructor
+    public Advisor(String FirstName, String LastName, int lecturerID, ArrayList<Student> students, ArrayList<Student> awaitingStudents, String password) {
+
+        super(FirstName, LastName, lecturerID);
+        if (students == null) {
+            this.students = new ArrayList<Student>();
+        }
+        else {
+            this.students = students;
+        }
+        this.awaitingStudents = findAwaitingStudents();
+        this.password = password;
+
+    }
+
+    // Find Students that send their requests to the Advisor
+    public ArrayList<Student> findAwaitingStudents() {
+
+        ArrayList<Student> awaitingStudent = new ArrayList<Student>();
+        int numberOfStudents = students.size();
+        for (int index = 0 ; index < numberOfStudents ; index++) {
+            Student student = students.get(index);
+            if (student.getRequest() == true) {
+                awaitingStudent.add(student);
+            }      
+        }
+        return awaitingStudent;
+    }
