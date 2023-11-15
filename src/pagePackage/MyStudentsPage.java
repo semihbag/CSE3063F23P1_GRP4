390a0caf57800218d8d41ee95ec0b3657026b584
semihbag <semih702bag@gmail.com>
2023-11-15 03:32:27 +0300

    my students page added

---
+++ src/pagePackage/MyStudentsPage.java
@@ -0,0 +1,20 @@
+package pagePackage;
+
+import systemMessagePackage.FunctionType;
+import systemMessagePackage.SystemMessage;
+
+public class MyStudentsPage extends Page {
+	
+	public MyStudentsPage(String content) {
+		super(content);
+		setType(PageType.MY_STUDENTS);
+		setName("All Students");
+	}
+	
+	@Override
+	public SystemMessage runPage() {
+		showContent();
+		takeInput();
+		return new SystemMessage(FunctionType.CHANGE_PAGE, PageType.MAIN_MENU_PAGE_ADVISOR, null);
+	}
+}
