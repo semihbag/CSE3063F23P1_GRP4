0125a19c4ff49675e4f4b675ba5743ff60b7d0f7
Aksanur Konuk <88140679+konukaksanur@users.noreply.github.com>
2023-11-15 17:03:00 +0300

    Create Password.java

---
+++ src/Password.java
@@ -0,0 +1,11 @@
+public class Password {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
