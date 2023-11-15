c78093622d5bdd044fa55ca4aacbc12faa4fe3ff
Aksanur Konuk <88140679+konukaksanur@users.noreply.github.com>
2023-11-15 17:21:35 +0300

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -2,19 +2,17 @@ abstract class Person {
     private String firstName;
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
