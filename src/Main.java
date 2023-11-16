922dcd65e49831a28c44743548bc001a92713dd8
Duru Baştunalı <durubastunali@gmail.com>
2023-11-16 13:48:52 +0300

    Main class is added for running.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws JSONException, IOException {
+        new SystemClass();
+    }
+}
