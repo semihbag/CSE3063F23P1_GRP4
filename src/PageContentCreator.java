edff1e1b5ce48f0a72deb924315b7da0b4ced5a7
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2023-11-16 11:20:54 +0000

    Created Main Menu PAge Student Content

---
+++ src/PageContentCreator.java
@@ -0,0 +1,15 @@
+public class PageContentCreator {
+
+    public static String crateMainMenuPageStudentContent() 
+	{
+		String str="---------MAIN MENU---------\n"+
+				    "1) All Courses\n"+
+				    "2) Offered Courses\n"+
+				    "3) Selected Courses\n"+
+				    "4) Log out\n"+
+				    "5) Exit";
+		return str;
+	}
+
+}
+
