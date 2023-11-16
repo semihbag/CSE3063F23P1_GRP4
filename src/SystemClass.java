63c78ba0b873d96afea3d8fe9c96151afb580d89
Duru Baştunalı <durubastunali@gmail.com>
2023-11-16 11:07:06 +0300

    Login method for user is added.

--- src/SystemClass.java
+++ src/SystemClass.java
@@ -9,17 +9,51 @@ import java.util.ArrayList;
 
 public class SystemClass {
     private SystemDomain domain;
-
+    private Person currentUser;
 
     public SystemClass() throws JSONException, IOException {
         domain = new SystemDomain();
-
+        UserInfo me = new UserInfo("o150120042","nida123");
+        login(me);
     }
 
+    public void login(UserInfo userInfo) {
+        boolean userFound = false;
+        if (userInfo.getUsername().charAt(0) == 'o') {
+            ArrayList<Student> students = domain.getStudents();
+            for (Student student : students) {
+                if (("o" + student.getStudentId().getId()).equals(userInfo.getUsername()) &&
+                        student.getPassword().getPassword().equals(userInfo.getPassword())) {
+                    setCurrentUser(student);
+                    userFound = true;
+                    // this.getUserInterface().setCurrentPage(PageType.MAIN_MENU_PAGE_STUDENT);
+                }
+            }
+        } else if (userInfo.getUsername().charAt(0) == 'a') {
+            ArrayList<Advisor> advisors = domain.getAdvisors();
+            for (Advisor advisor : advisors) {
+                if (("o" + advisor.getId().getId()).equals(userInfo.getUsername()) &&
+                        advisor.getPassword().getPassword().equals(userInfo.getPassword())) {
+                    setCurrentUser(advisor);
+                    userFound = true;
+                    // this.getUserInterface().setCurrentPage(PageType.MAIN_MENU_PAGE_ADVISOR);
+                }
+            }
+        } if (!userFound) {
+            System.out.println("Username/Password incorrect.");
+        }
+    }
 
     public SystemDomain getDomain() {
         return domain;
     }
 
+    public Person getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(Person currentUser) {
+        this.currentUser = currentUser;
+    }
 }
 
