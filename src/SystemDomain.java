3e5d9997224eb9944dc78c600c474adf6b3e3136
Duru Baştunalı <durubastunali@gmail.com>
2023-12-08 10:18:52 +0300

    setStudentCourses method is optimized.

--- src/SystemDomain.java
+++ src/SystemDomain.java
@@ -107,24 +107,18 @@ public class SystemDomain {
         }
     }
 
-    //Assign variable course types to students
     private ArrayList<Course> setStudentCourses(String[] studentCoursesAr) {
         ArrayList<Course> studentCoursesList = new ArrayList<>();
-        for (int i = 0; i < studentCoursesAr.length; i++) {
+        for (String s : studentCoursesAr) {
             for (int j = 0; j < getCourses().size(); j++) {
-                if(getCourses().get(j) instanceof CourseSession){
-                    if((getCourses().get(j).getCourseId().getId()+"."+((CourseSession)getCourses().get(j)).getSessionId().getId()).equals(studentCoursesAr[i])){
-                        studentCoursesList.add(getCourses().get(j));
-                    }
-                } else {
-                    if (studentCoursesAr[i].equals(getCourses().get(j).getCourseId().getId())) {
-                        studentCoursesList.add(getCourses().get(j));
+                if ((getCourses().get(j) instanceof CourseSession &&
+                        (getCourses().get(j).getCourseId().getId() + "." + ((CourseSession) getCourses().get(j)).getSessionId().getId()).equals(s))
+                        || s.equals(getCourses().get(j).getCourseId().getId())) {
+                    studentCoursesList.add(getCourses().get(j));
 
-                    }
                 }
             }
-        }
-        return studentCoursesList;
+        } return studentCoursesList;
     }
 
     //Create all courses
