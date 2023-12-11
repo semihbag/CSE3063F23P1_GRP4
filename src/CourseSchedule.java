3c15c2c520b5345d3d8c512b8db92668ed5a92ff
erenncetin <147340241+erenncetin@users.noreply.github.com>
2023-12-11 21:19:09 +0300

    CourSchedule Class is added

---
+++ src/CourseSchedule.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class CourseSchedule {
+
+    private Day courseDay;
+    private ArrayList<Hour> courseHours;
+
+    public CourseSchedule(Day courseDays,ArrayList<Hour> courseHours) {
+        this.courseDay = courseDays;
+        this.courseHours = courseHours;
+    }
+
+    public Day getCourseDay() {
+        return courseDay;
+    }
+
+    public void setCourseDay(Day courseDay) {
+        this.courseDay = courseDay;
+    }
+
+    public ArrayList<Hour> getCourseHours() {
+        return courseHours;
+    }
+
+    public void setCourseHours(ArrayList<Hour> courseHours) {
+        this.courseHours = courseHours;
+    }
+
+    
+}
