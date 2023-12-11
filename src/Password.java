6d8feb787e573a13608c9ac27f787cf6d34cfc5f
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2023-12-11 23:38:30 +0300

    updated parametres for passwordmethods

--- src/Password.java
+++ src/Password.java
@@ -13,35 +13,35 @@ public class Password {
         this.password = password;
     }
 
-    public boolean comparePreviousPassword(Password newPassword){
-        return newPassword.equals(this.password);
+    public boolean comparePreviousPassword(String newPassword){
+        return newPassword.equals(password);
     }
 
-    public boolean checkPasswordCond(Password password){
+    public boolean checkPasswordCond(String newPassword){
 		boolean containsUpperCase = false, containsLowerCase = false;
 		boolean containsNumber = false;
         boolean valid=false;
 
-        for (char c : password.getPassword().toCharArray()) {
+        for (char c : newPassword.toCharArray()) {
             if (Character.isDigit(c)) {
                 containsNumber = true;
                 break;
             }
         }
-        for (char c : password.getPassword().toCharArray()) {
+        for (char c : newPassword.toCharArray()) {
             if (Character.isUpperCase(c)) {
                 containsUpperCase = true;
                 break;
             }
         }
-        for (char c : password.getPassword().toCharArray()) {
+        for (char c : newPassword.toCharArray()) {
             if (Character.isLowerCase(c)) {
                 containsLowerCase = true;
                 break;
             }
         }
-		if ((password.getPassword().length() > 8 || password.getPassword().length() < 20)&&
-			(password.getPassword().contains(".") || password.getPassword().contains("_"))&&
+		if ((newPassword.length() > 8 || newPassword.length() < 20)&&
+			(newPassword.contains(".") || newPassword.contains("_"))&&
 			containsUpperCase && containsLowerCase && containsNumber){
 				valid=true;
 			}
