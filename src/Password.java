d4090675f1d3a953c61d6b01f8c0636ce042f774
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2023-12-11 23:13:56 +0300

    Added checking password methods

--- src/Password.java
+++ src/Password.java
@@ -12,4 +12,40 @@ public class Password {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public boolean comparePreviousPassword(Password newPassword){
+        return newPassword.equals(this.password);
+    }
+
+    public boolean checkPasswordCond(Password password){
+		boolean containsUpperCase = false, containsLowerCase = false;
+		boolean containsNumber = false;
+        boolean valid=false;
+
+        for (char c : password.getPassword().toCharArray()) {
+            if (Character.isDigit(c)) {
+                containsNumber = true;
+                break;
+            }
+        }
+        for (char c : password.getPassword().toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                containsUpperCase = true;
+                break;
+            }
+        }
+        for (char c : password.getPassword().toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                containsLowerCase = true;
+                break;
+            }
+        }
+		if ((password.getPassword().length() > 8 || password.getPassword().length() < 20)&&
+			(password.getPassword().contains(".") || password.getPassword().contains("_"))&&
+			containsUpperCase && containsLowerCase && containsNumber){
+				valid=true;
+			}
+			
+         return valid;
+	}
 }
