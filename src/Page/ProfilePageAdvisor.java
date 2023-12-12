63b2d46da931d30038ad65a643dc19fdb55ffe31
semihbag <semih702bag@gmail.com>
2023-12-12 14:28:34 +0300

    profile page advisor completed

--- src/Page/ProfilePageAdvisor.java
+++ src/Page/ProfilePageAdvisor.java
@@ -1,16 +1,32 @@
 package Page;
 
+import SystemMessage.FunctionType;
 import SystemMessage.SystemMessage;
 
 public class ProfilePageAdvisor extends Page{
 	
 	public ProfilePageAdvisor(String content) {
 		super(content);
+		setType(PageType.PROFILE_PAGE_ADVISOR);
+		setName("Profile Page Advisor");
 		
 	}
 	
 	@Override 
 	public SystemMessage runPage() {
-		return null;
+		showContent();
+		String input;
+		while (true) {
+			input = takeInput();
+			if (input.equalsIgnoreCase("c")) {
+				return new SystemMessage(FunctionType.CHANGE_PAGE, PageType.CHANGE_PASSWORD_PAGE, null);
+			}
+			else if (input.equalsIgnoreCase("q")) {
+				return new SystemMessage(FunctionType.CHANGE_PAGE, PageType.MAIN_MENU_PAGE_ADVISOR, null);
+			}
+			else {
+				System.out.println("Wrong Input");
+			}
+		}
 	}
 }
