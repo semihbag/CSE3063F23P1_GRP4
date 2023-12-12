439e81dc3dceff59c4e65288722e3dd4d6099ddb
semihbag <semih702bag@gmail.com>
2023-12-12 11:52:50 +0300

    profile page advisor added

---
+++ src/Page/ProfilePageAdvisor.java
@@ -0,0 +1,16 @@
+package Page;
+
+import SystemMessage.SystemMessage;
+
+public class ProfilePageAdvisor extends Page{
+	
+	public ProfilePageAdvisor(String content) {
+		super(content);
+		
+	}
+	
+	@Override 
+	public SystemMessage runPage() {
+		return null;
+	}
+}
