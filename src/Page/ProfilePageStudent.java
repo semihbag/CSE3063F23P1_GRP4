11c056615a0e6919331123fd56e702c2337a8ac0
semihbag <semih702bag@gmail.com>
2023-12-12 14:24:20 +0300

    profile page student completed

--- src/Page/ProfilePageStudent.java
+++ src/Page/ProfilePageStudent.java
@@ -1,16 +1,31 @@
 package Page;
 
+import SystemMessage.FunctionType;
 import SystemMessage.SystemMessage;
 
 public class ProfilePageStudent extends Page{
 	
 	public ProfilePageStudent(String content) {
 		super(content);
-		
+		setType(PageType.PROFILE_PAGE_STUDENT);
+		setName("Profile Page Student");
 	}
 	
 	@Override 
 	public SystemMessage runPage() {
-		return null;
+		showContent();
+		String input;
+		while (true) {
+			input = takeInput();
+			if (input.equalsIgnoreCase("c")) {
+				return new SystemMessage(FunctionType.CHANGE_PAGE, PageType.CHANGE_PASSWORD_PAGE, null);
+			}
+			else if (input.equalsIgnoreCase("q")) {
+				return new SystemMessage(FunctionType.CHANGE_PAGE, PageType.MAIN_MENU_PAGE_STUDENT, null);
+			}
+			else {
+				System.out.println("Wrong Input");
+			}
+		}
 	}
 }
