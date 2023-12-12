e20516f43dc2f07ad047311baa1e2ae5e254e50d
semihbag <semih702bag@gmail.com>
2023-12-12 11:51:50 +0300

    student profile page added

---
+++ src/Page/ProfilePageStudent.java
@@ -0,0 +1,16 @@
+package Page;
+
+import SystemMessage.SystemMessage;
+
+public class ProfilePageStudent extends Page{
+	
+	public ProfilePageStudent(String content) {
+		super(content);
+		
+	}
+	
+	@Override 
+	public SystemMessage runPage() {
+		return null;
+	}
+}
