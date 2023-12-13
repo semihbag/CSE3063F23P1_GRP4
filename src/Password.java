a35cd94bfb11b1a5bd2fdbfbd50e2beac665b55c
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2023-12-13 13:41:03 +0300

    updated Password Class

--- src/Password.java
+++ src/Password.java
@@ -13,39 +13,70 @@ public class Password {
         this.password = password;
     }
 
-    public boolean comparePreviousPassword(String newPassword){
-        return newPassword.equals(password);
+    public boolean compareCurrentPassword(String enteredPassword){
+        return enteredPassword.equals(password);
     }
 
     public boolean checkPasswordCond(String newPassword){
-		boolean containsUpperCase = false, containsLowerCase = false;
-		boolean containsNumber = false;
-        boolean valid=false;
-
-        for (char c : newPassword.toCharArray()) {
-            if (Character.isDigit(c)) {
-                containsNumber = true;
-                break;
-            }
-        }
-        for (char c : newPassword.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                containsUpperCase = true;
-                break;
-            }
-        }
-        for (char c : newPassword.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                containsLowerCase = true;
-                break;
-            }
-        }
-		if ((newPassword.length() > 8 && newPassword.length() < 20)&&
-			(newPassword.contains(".") || newPassword.contains("_"))&&
-			containsUpperCase && containsLowerCase && containsNumber){
+		boolean containsUpperCase = checkUpperCaseCond(newPassword),
+        containsLowerCase =checkUpperCaseCond(newPassword),
+	    containsNumber = checkUpperCaseCond(newPassword),
+        continsSpeacialCharacter=isContains(newPassword),
+        lenght=lenghtCond(newPassword),
+        valid=false;  
+		if (lenght && continsSpeacialCharacter && containsUpperCase && containsLowerCase && containsNumber){
 				valid=true;
 			}
-			
          return valid;
 	}
+
+    public boolean isContains(String newPassword){
+        boolean valid=false;
+        if(newPassword.contains(".") || newPassword.contains("_")){
+            valid=true;
+        }
+        return valid;
+    }
+
+    public boolean lenghtCond(String newPassword){
+        boolean valid=false;
+        if(newPassword.length() > 8 && newPassword.length() < 20){
+            valid=true;
+        }
+        return valid;
+    }
+
+    public boolean checkUpperCaseCond(String newPassword){
+        boolean valid=false;
+        for (char c : newPassword.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                valid = true;
+                break;
+            }
+        }
+        return valid;
+    }
+
+    public boolean checkLowerCaseCond(String newPassword){
+        boolean valid=false;
+         for (char c : newPassword.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                valid = true;
+                break;
+            }
+        }
+        return valid;
+    }
+
+    public boolean checkDigitCaseCond(String newPassword){
+        boolean valid=false;
+         for (char c : newPassword.toCharArray()) {
+            if (Character.isDigit(c)) {
+                valid = true;
+                break;
+            }
+        }
+        return valid;
+    }
+    
 }
