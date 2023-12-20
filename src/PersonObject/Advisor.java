756e977104acb91f47e032a1709a605c45d2d379
semihbag <semih702bag@gmail.com>
2023-12-20 14:00:10 +0300

    Merge branch 'ITERATION2-V2.0'

--- src/PersonObject/Advisor.java
+++ src/PersonObject/Advisor.java
@@ -9,12 +9,7 @@ public class Advisor extends Lecturer {
 
     // Constructor
     public Advisor(String FirstName, String LastName, Id lecturerID, Password password) {
-<<<<<<<< HEAD:iter1/src/Advisor.java
-
-        super(FirstName, LastName, lecturerID);
-========
         super(FirstName, LastName, lecturerID,password);
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
         this.selectStudent = null;
     }
 
@@ -23,15 +18,9 @@ public class Advisor extends Lecturer {
         int numberOfStudents = students.size();
         for (int index = 0 ; index < numberOfStudents ; index++) {
             Student student = students.get(index);
-<<<<<<<< HEAD:iter1/src/Advisor.java
-            if (student.getRequest().equals("true")) {
-                awaitingStudents.add(student);
-            }      
-========
             if (!awaitingStudents.contains(student) && student.getRequest().equals("true")) {
                 awaitingStudents.add(student);
             }           
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
         }
     }
 
@@ -42,13 +31,8 @@ public class Advisor extends Lecturer {
         this.setSelectStudent(currentStudent);
     }
 
-<<<<<<<< HEAD:iter1/src/Advisor.java
-// Approve request of selected Student
-    public void Approve() {
-========
     // Approve request of selected Student
     public void approve() {
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
         ArrayList<Course> selectCourses = this.selectStudent.getSelectedCourses();
         int numberOfCourses = selectCourses.size();
         for(int index = 0 ; index < numberOfCourses ; index++) {
@@ -63,22 +47,13 @@ public class Advisor extends Lecturer {
     }
 
     // Dissapprove Student request
-<<<<<<<< HEAD:iter1/src/Advisor.java
-    public void Disapprove() {
-========
     public void disapprove() {
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
         ArrayList<Course> selectCourses = this.selectStudent.getSelectedCourses();
         int numberOfCourses = selectCourses.size();
         for(int index = 0 ; index < numberOfCourses ; index++) {
             Course course = selectCourses.get(index);
-<<<<<<<< HEAD:iter1/src/Advisor.java
-            this.selectStudent.addSelectableCourse(course);
-            course.setQuota(course.getQuota() + 1);
-========
             course.setQuota(course.getQuota() + 1);
             this.selectStudent.getSyllabus().removeCourseFromSyllabus(course);
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
         }
         this.selectStudent.dropAllSelectedCourses();
         this.selectStudent.setRequest("false");
@@ -122,11 +97,7 @@ public class Advisor extends Lecturer {
     }
     
     // After approve or disapprove, update the awaitingStudent 
-<<<<<<<< HEAD:iter1/src/Advisor.java
-    public void removeAwaitingStudent(Student student) {
-========
     private void removeAwaitingStudent(Student student) {
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
     	int ind = awaitingStudents.indexOf(student);
         this.awaitingStudents.remove(ind);
     }
@@ -139,11 +110,5 @@ public class Advisor extends Lecturer {
         this.selectStudent = student;
     }
     
-<<<<<<<< HEAD:iter1/src/Advisor.java
-    public Password getPassword() {
-        return password;
-    }
-========
 
->>>>>>>> ITERATION2-V2.0:src/PersonObject/Advisor.java
 }
