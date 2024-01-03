65564ec3b781a1a4bd9f77ba4598d1161e23986e
semihbag <semih702bag@gmail.com>
2024-01-03 18:45:44 +0300

    Create MyNotificationsPage.py

---
+++ python_development/Page/MyNotificationsPage.py
@@ -0,0 +1,20 @@
+from Page import Page
+from Page import PageType
+from System import FunctionType
+from System import SystemMessage
+
+class MyNotificationsPage(Page):
+
+    def __init__(self, content):
+        super().__init__(content)
+        self.setType(PageType.MyNotificationsPage)
+        self.setName("My Notifications Page")
+
+
+    def runPage(self):
+        self.showContent()
+        self.takeInput()
+        return SystemMessage(FunctionType.READ_NOTIFICATIONS, PageType.MAIN_MENU_PAGE, None)
+    
+
+    
