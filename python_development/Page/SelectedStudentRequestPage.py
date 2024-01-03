fc3fcc8342c82a50858761cce2f007e73dcf8981
semihbag <semih702bag@gmail.com>
2024-01-03 19:53:58 +0300

    Create SelectedStudentRequestPage.py

---
+++ python_development/Page/SelectedStudentRequestPage.py
@@ -0,0 +1,30 @@
+from Page import Page
+from Page import PageType
+from System import FunctionType
+from System import SystemMessage
+
+class SelectedStudentRequestPage(Page):
+     
+    def __init__(self, content):
+        super().__init__(content)
+        self.setType(PageType.SELECTED_STUDENT_REQUEST_PAGE)
+        self.setName("Selected Student Request Page")
+
+
+    def runPage(self):
+        self.showContent()
+        inputFromPage = None
+        while(True):
+            inputFromPage = self.takeInput()
+            if(inputFromPage.lower() == "a"):
+                print("Type your message or press enter directly:")
+                message = self.takeInput()
+                return SystemMessage(FunctionType.APPROVE_REQUEST, PageType.EVALUATE_REQUESTS_PAGE, message)
+            elif (inputFromPage.lower() == "r"):
+                print("Type your message or press enter directly:")
+                message = self.takeInput()
+                return SystemMessage(FunctionType.DISAPPREOVE_REQUEST, PageType.EVALUATE_REQUESTS_PAGE, message)
+            elif (inputFromPage.lower() == "q"):
+                return SystemMessage(FunctionType.CHANGE_PAGE, PageType.EVALUATE_REQUESTS_PAGE, None)
+            else:
+                print("Wrong Input!")
