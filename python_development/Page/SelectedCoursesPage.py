10d79bc5d61056c7bc6bb7085e3e1eb32323ff75
semihbag <semih702bag@gmail.com>
2024-01-03 19:45:04 +0300

    Create SelectedCoursesPage.py

---
+++ python_development/Page/SelectedCoursesPage.py
@@ -0,0 +1,41 @@
+from Page import Page
+from Page import PageType
+from System import FunctionType
+from System import SystemMessage
+
+class SelectedCoursesPage(Page):
+
+    numberOfDropableCourses = None
+
+    def __init__(self, content):
+        super().__init__(content)
+        self.setType(PageType.SelectedCoursesPage)
+        self.setName("Selected Courses Page")
+
+
+    def runPage(self):
+        self.showContent()
+        inputFromPage = None
+        while(True):
+            inputFromPage = self.takeInput()
+            try:
+                selection = int(inputFromPage)
+                if (selection > self.numberOfDropableCourses or selection < 0):
+                    print("Enter a valid number")
+                else:
+                    return SystemMessage(FunctionType.DROP_COURSE, PageType.SELECTED_COURSES_PAGE, selection)
+            except:
+                if(inputFromPage.lower() == "a"):
+                    return SystemMessage(FunctionType.SEND_APPROVE, PageType.MAIN_MENU_PAGE, None)
+                if(inputFromPage.lower() == "q"):
+                    return SystemMessage(FunctionType.CHANGE_PAGE, PageType.MAIN_MENU_PAGE, None)
+                else:
+                    print("Wrong Input")
+
+
+    def getNumberOfDropableCourses(self):
+        return self.numberOfDropableCourses
+    
+    def setNumberOfDropableCourses(self, numberOfDropableCourses):
+        self.numberOfDropableCourses = numberOfDropableCourses
+        
