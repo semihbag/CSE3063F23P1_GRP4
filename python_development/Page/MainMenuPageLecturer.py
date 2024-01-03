65ede516a7602ffb11116a50da7843bb35c06fb7
semihbag <semih702bag@gmail.com>
2024-01-03 18:24:36 +0300

    Create MainMenuPageLecturer.py

---
+++ python_development/Page/MainMenuPageLecturer.py
@@ -0,0 +1,30 @@
+from Page import Page
+from Page import PageType
+from System import FunctionType
+from System import SystemMessage
+
+class MainMenuPageLecturer(Page):
+
+    def __init__(self, content):
+        super().__init__(content)
+        self.setType(PageType.MAIN_MENU_PAGE)
+        self.setName("Main Menu Page Lecturer")
+
+
+    def runPage(self):
+        self.showContent()
+        while(True):
+            inputFromPage = self.takeInput()
+
+            if (inputFromPage == "1"):
+                return SystemMessage(FunctionType.CHANGE_PAGE, PageType.PROFILE_PAGE, None)
+            elif (inputFromPage == "2"):
+                return SystemMessage(FunctionType.CHANGE_PAGE, PageType.MY_COURSES_PAGE, None)
+            elif (inputFromPage == "3"):
+                return SystemMessage(FunctionType.CHANGE_PAGE, PageType.SYLLABUS_PAGE, None)
+            elif (inputFromPage == "4"):
+                return SystemMessage(FunctionType.LOGOUT, PageType.LOGIN_PAGE, None)
+            elif (inputFromPage == "5"):
+                return SystemMessage(FunctionType.EXIT, None, None);
+            else:
+               print("Wrong Input!")
