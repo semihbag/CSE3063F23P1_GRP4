ee72ec397ff901d74b322f9d8879fee1d9967e9f
Duru Baştunalı <durubastunali@gmail.com>
2024-01-04 11:52:55 +0300

    system domain added

---
+++ python_development/System/SystemDomain.py
@@ -0,0 +1,28 @@
+from python_development.Creator.CreateAdvisor import CreateAdvisor
+from python_development.Creator.CreateLecturer import CreateLecturer
+from python_development.Creator.CreateStudent import CreateStudent
+
+
+class SystemDomain:
+    def __init__(self):
+        self.lecturerCreator = CreateLecturer("JSON_Files\\lecturers.json")
+        self.advisorCreator = CreateAdvisor("JSON_Files\\advisors.json", self.lecturerCreator.getLecturers())
+        self.courseCreator = CreateCourse("src/JSON_Files/courses.json", self.lecturerCreator.getLecturers())
+        self.studentCreator = CreateStudent("JSON_Files\\Students\\", "JSON_Files\\student_json.txt",
+                                             self.courseCreator.getCourses(), self.advisorCreator.getAdvisors())
+        self.pageCreator = CreatePage()
+
+    def getLecturerCreator(self):
+        return self.lecturerCreator
+
+    def getAdvisorCreator(self):
+        return self.advisorCreator
+
+    def getCourseCreator(self):
+        return self.courseCreator
+
+    def getStudentCreator(self):
+        return self.studentCreator
+
+    def getPageCreator(self):
+        return self.pageCreator
